4c6e2226d10ddb2a7f979439a58920c73cede18c
kickingdude <kameronferrer8@gmail.com>
2023-12-18 17:10:50 -0800

    User Management changes

--- Natural Selection/CraftVerify Documents/Milestone 2/Coding (includes Automated Test)/User Management/userModification_automatedTesting.cs
+++ Natural Selection/CraftVerify Documents/Milestone 2/Coding (includes Automated Test)/User Management/userModification_automatedTesting.cs
@@ -36,6 +36,67 @@ public class UserModificationTests
     }
 
     [TestMethod]
+    public void UpdateUserProfile_ValidBioAndPNGPhoto_ReturnsTrue()
+    {
+        // Arrange
+        var userMod = new UserModification();
+        string validBio = "This is a valid bio.";
+        byte[] validPNGPhoto = new byte[] { 0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A }; // Mock PNG header
+
+        // Act
+        bool result = userMod.UpdateUserProfile("testUserId", validBio, validPNGPhoto);
+
+        // Assert
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void UpdateUserProfile_ValidBioAndInvalidPNGPhoto_ReturnsFalse()
+    {
+        // Arrange
+        var userMod = new UserModification();
+        string validBio = "This is a valid bio.";
+        byte[] invalidPNGPhoto = new byte[] { 0xFF, 0xD8 }; // Incorrect header for PNG
+
+        // Act
+        bool result = userMod.UpdateUserProfile("testUserId", validBio, invalidPNGPhoto);
+
+        // Assert
+        Assert.IsFalse(result);
+    }
+
+
+    [TestMethod]
+    public void UpdateUserProfile_ValidBioAndHEIFPhoto_ReturnsTrue()
+    {
+        // Arrange
+        var userMod = new UserModification();
+        string validBio = "This is a valid bio.";
+        byte[] validHEIFPhoto = new byte[] { 0x00, 0x00, 0x00, 0x18, 0x66, 0x74, 0x79, 0x70, 0x68, 0x65, 0x69, 0x63 }; // Mock HEIF ftyp box header
+
+        // Act
+        bool result = userMod.UpdateUserProfile("testUserId", validBio, validHEIFPhoto);
+
+        // Assert
+        Assert.IsTrue(result);
+    }
+
+    [TestMethod]
+    public void UpdateUserProfile_ValidBioAndInvalidHEIFPhoto_ReturnsFalse()
+    {
+        // Arrange
+        var userMod = new UserModification();
+        string validBio = "This is a valid bio.";
+        byte[] invalidHEIFPhoto = new byte[] { 0x00, 0x00, 0x00, 0x18, 0xFF, 0xD8 }; // Incorrect header for HEIF
+
+        // Act
+        bool result = userMod.UpdateUserProfile("testUserId", validBio, invalidHEIFPhoto);
+
+        // Assert
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
     public void UpdateUserProfile_InvalidPhoto_ReturnsFalse()
     {
         // Arrange
@@ -102,20 +163,6 @@ public class UserModificationTests
 
         // Act
         bool result = userMod.ValidateProfilePhoto(oversizedPhoto);
-
-        // Assert
-        Assert.IsFalse(result);
-    }
-
-    [TestMethod]
-    public void ValidateProfilePhoto_InvalidFormat_ReturnsFalse()
-    {
-        // Arrange
-        var userMod = new UserModification();
-        byte[] invalidFormatPhoto = { 0x00 }; // Mock invalid format
-
-        // Act
-        bool result = userMod.ValidateProfilePhoto(invalidFormatPhoto);
 
         // Assert
         Assert.IsFalse(result);
